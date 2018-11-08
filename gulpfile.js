0f73b031aae067482f790cbcb44d0b1748bbb68d
wengeek <wenwei897684475@gmail.com>
2018-11-08 17:41:04 +0800

    Deploy website
    Deploy website version based on b9274db0c7614b0f8be822b247051f097b668b54

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require('gulp')
-const ghPages = require('gulp-gh-pages')
-
-//Deploy
-gulp.task('deploy', function() {
-  return gulp.src('./_book/**/*')
-    .pipe(ghPages())
-})
---
+++ js/i18n.js
@@ -0,0 +1,4 @@
+(function () {
+  var i18n = {}
+  // @TODO: cache i18n
+})()
